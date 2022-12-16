d5e46ea27c26125aa8313907eef0cee238cef3d9
Ingvar Derešivski <inde999@gmail.com>
2022-12-16 12:08:15 +0200

    16.12.2022 Firstlesson

---
+++ 16.12.2022IfjaElse/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "If ja else";
+            Console.WriteLine("Kasutaja kontroll");
+            Console.WriteLine("Palun sisesta oma nimi: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Palun sisesta vanus: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tere {0}, oled {1} aastane.", name, age);
+            //muutujad on taga
+            Console.WriteLine("Tere " + name + ", oled " + age + " aastane.");
+            //muutujad on stringi vahel
+            Console.WriteLine($"Tere {name}, oled {age} aastane."); 
+            //string format
+
+            if (age < 7)
+            {
+                Console.WriteLine("Loodan, et sa käid juba koolis.");
+            }
+            else if (age >= 7 && age <= 18)
+            {
+                Console.WriteLine("Nüüd peaksid kindlasti koolis käima.");
+            }
+            else if (age >= 19 && age <= 75)
+            {
+                Console.WriteLine("Oled vist juba tööl.");
+            }
+            else if (age >= 75 && age < 120)
+            {
+                Console.WriteLine("Loodan, et puhkad kuskil soojal saarel.");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ristküliku pindala ja ümbermõõt!");
+            Console.WriteLine("Sisesta esimese külje pikkus:");
+
+            string firstLength = Console.ReadLine();
+            float firstLengthParsed = float.Parse(firstLength);
+
+            Console.WriteLine("Sisesta teise külje pikkus:");
+            double secondLength = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ristküliku ümbermõõt " + 2 * (firstLengthParsed + secondLength));
+            Console.WriteLine("Ristküliku pindala " + (firstLengthParsed * secondLength));
+
+            Console.ReadKey();
+        }
+    }
+}
