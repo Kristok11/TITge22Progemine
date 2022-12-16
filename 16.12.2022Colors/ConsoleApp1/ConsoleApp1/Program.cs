fd87e117dd54455aeae500ec04ba19282b0d1e7d
Ingvar Derešivski <inde999@gmail.com>
2022-12-16 12:34:55 +0200

    IfandElseColors

---
+++ 16.12.2022Colors/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta nimi");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Tere " + name + ", nüüd hakkab värvimuusika");
+
+            if (name != "")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Tere " + name);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("Nimi ei ole sisestatud. Head aega!");
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+            }
+            Console.ReadKey();
+        }
+    }
+}
