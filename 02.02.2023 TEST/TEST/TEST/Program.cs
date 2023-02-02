60608c6fa024219796ebfad137bfc16e830a6d9c
Ingvar Derešivski <inde999@gmail.com>
2023-02-02 12:21:19 +0200

    02.02.2023 TEST

---
+++ 02.02.2023 TEST/TEST/TEST/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.NetworkInformation;
+
+namespace TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Koosta neli funktsiooni, millest igaüks arvutab ja 
+            //väljastab ühe geomeetrilise kujundi(ruut, 
+            //ring või teemant, ristkylik, kolmnurk) 
+            //kujundi "*" sümbolina(vastavalt võimalusele 
+            //arvutab ja väljastab viimaks ka ümbermõõdu, 
+            //pindala jne).Loo programm, mis küsib kasutajalt 
+            //kujundi tüüpi sõna valikuna, kujundi andmeid
+            //(vastavalt vajadusele külje pikkust, raadiust vms).
+            //NB! Teemanti puhul arvutage ringi ümbermõõt ja pindala.
+
+            Console.WriteLine("Sisesta kujund: ");
+            Console.WriteLine("1. Ruut");
+            Console.WriteLine("2. Teemant");
+            Console.WriteLine("3. Ristkülik");
+            Console.WriteLine("4. Kolmnurk");
+
+            string tehe = Console.ReadLine();
+
+            switch (tehe)
+            {
+                case "kujund":
+                    Square();
+                    break;
+                default:
+                    Console.WriteLine("Valikut ei tehtud");
+                    break;
+            }
+        }
+
+
+        private static void Square()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("ruut");
+            Console.WriteLine(number);
+        }
+    }
+}
